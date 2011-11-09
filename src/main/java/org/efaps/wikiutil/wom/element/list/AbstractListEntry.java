92beafe62d4d133674423febf43e26eaaf6e2f8b
tim@moxter.net <tim@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2011-11-09 23:28:08 +0000

    - checkstyle fix: remove () of @Override annotation

--- src/main/java/org/efaps/wikiutil/wom/element/list/AbstractListEntry.java
+++ src/main/java/org/efaps/wikiutil/wom/element/list/AbstractListEntry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2010 The eFaps Team
+ * Copyright 2003 - 2011 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -86,7 +86,7 @@ public abstract class AbstractListEntry<ELEM extends AbstractListEntry<?>>
      *
      * @return string representation
      */
-    @Override()
+    @Override
     public String toString()
     {
         return new ToStringBuilder(this)
